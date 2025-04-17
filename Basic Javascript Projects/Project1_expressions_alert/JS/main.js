8fc0dfb595ffc32db9e3cc8e50fe1ecbdb753205
jennatheexplora <jennavb.1107@gmail.com>
2025-04-17 17:18:13 -0400

    Initial commit of basic js projects 2; functions.

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,11 @@
 window.alert("Hello, world"); //creates a popup window upon the page opening//
 
-var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string."; //assigning values (string type) to variables//
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string. "; //assigning values (string type) to variables//
 var fullSent = Sent1 + Sent2; //concatenates the two strings//
 document.write(fullSent); //displays the text of var fullSent on the page//
 
 document.write(2 + 17); //an expression//
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  } //practuce using an event//
